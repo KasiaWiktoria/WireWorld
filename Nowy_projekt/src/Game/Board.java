9b35de418717203c271b82f2298120b2895e6771
KasiaWiktoria <kw_s@interia.pl>
2019-05-08 23:32:15 +0200

    uaktualnienie interfejsu, dodanie funkcji do klasy Board

--- Nowy_projekt/src/Game/Board.java
+++ Nowy_projekt/src/Game/Board.java
@@ -1,6 +1,12 @@
 package Game;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+
+import java.util.Random;
 
 public class Board {
 
@@ -18,6 +24,81 @@ public class Board {
         else
             this.cellSize = 600./(double)this.rows;
     }
+
+    public void colorToolFill(Canvas colorTool, int colorToolSize){
+        GraphicsContext colorToolGC = colorTool.getGraphicsContext2D();
+        colorToolGC.setFill(Color.RED);
+        colorToolGC.fillRect(0,0, colorToolSize, colorToolSize);
+        colorToolGC.setFill(Color.YELLOW);
+        colorToolGC.fillRect(0, colorToolSize, colorToolSize, colorToolSize);
+        colorToolGC.setFill(Color.BLUE);
+        colorToolGC.fillRect(colorToolSize,0, colorToolSize, colorToolSize);
+        colorToolGC.setFill(Color.BLACK);
+        colorToolGC.fillRect(colorToolSize, colorToolSize, colorToolSize, colorToolSize);
+    }
+
+    public void bwToolFill(Canvas bwTool, int bwToolSize) {
+        GraphicsContext bwToolGC = bwTool.getGraphicsContext2D();
+        bwToolGC.setFill(Color.BLACK);
+        bwToolGC.fillRect(0, 0, bwToolSize, bwToolSize);
+        bwToolGC.setFill(Color.WHITE);
+        bwToolGC.fillRect(bwToolSize, 0, bwToolSize, bwToolSize);
+    }
+
+    public void golCanvasFill(Canvas golCanvas) {
+        Random golGenerator = new Random();
+        GraphicsContext golCanvasGC = golCanvas.getGraphicsContext2D();
+        golCanvasGC.setFill(Color.BLACK);
+        for (int i = 0; i < this.getRows(); i++) {
+            for (int j = 0; j < this.getColumns(); j++) {
+                if (golGenerator.nextBoolean() == true)
+                    golCanvasGC.setFill(Color.WHITE);
+                else
+                    golCanvasGC.setFill(Color.BLACK);
+                golCanvasGC.fillRect(j * this.cellSize, this.cellSize * i, this.cellSize, this.cellSize);
+            }
+        }
+    }
+
+    public void canvasBlackFill(Canvas wwCanvas) {
+        GraphicsContext wwCanvasGC = wwCanvas.getGraphicsContext2D();
+        for (int i = 0; i < this.getRows(); i++) {
+            for (int j = 0; j < this.getColumns(); j++) {
+                wwCanvasGC.setFill(Color.BLACK);
+                wwCanvasGC.fillRect(j * this.cellSize, this.cellSize * i, this.cellSize, this.cellSize);
+            }
+        }
+    }
+
+    public void wwCanvasRandomFill(Canvas wwCanvas) {
+        GraphicsContext wwCanvasGC = wwCanvas.getGraphicsContext2D();
+        Random wwGenerator = new Random();
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 32; j++) {
+                if (wwGenerator.nextInt() % 4 == 0)
+                    wwCanvasGC.setFill(Color.RED);
+                else if (wwGenerator.nextInt() % 4 == 1)
+                    wwCanvasGC.setFill(Color.YELLOW);
+                else if (wwGenerator.nextInt() % 4 == 2)
+                    wwCanvasGC.setFill(Color.BLUE);
+                else
+                    wwCanvasGC.setFill(Color.BLACK);
+                wwCanvasGC.fillRect(j * this.cellSize, this.cellSize * i, this.cellSize, this.cellSize);
+            }
+        }
+    }
+
+    public void setDimension(TextField columns, TextField rows) {
+        this.setColumns(Integer.parseInt(columns.getText()));
+        this.setRows(Integer.parseInt(rows.getText()));
+    }
+
+    public void setPromptForDimensions(TextField columns, TextField rows) {
+        columns.setPromptText("columns");
+        rows.setPromptText("rows");
+    }
+
+    //Getters and Setters methods
     public void setColumns(int columns) {
         this.columns = columns;
     }
@@ -41,5 +122,6 @@ public class Board {
     public Canvas getCanvas() {
         return canvas;
     }
-
 }
+
+
--- Nowy_projekt/src/Game/CanvasController.java
+++ Nowy_projekt/src/Game/CanvasController.java
@@ -4,20 +4,17 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
-
-import java.awt.*;
 import java.util.Random;
 
 public class CanvasController {
 
-
     @FXML
-    Canvas colorCanvas;
+    Canvas wwCanvas;
     @FXML
-    Canvas bwCanvas;
+    Canvas golCanvas;
     @FXML
     Canvas colorTool;
     int colorToolSize = 40;
@@ -25,81 +22,32 @@ public class CanvasController {
     Canvas bwTool;
     int bwToolSize = 40;
     @FXML
-    TextField columns;
+    TextField golColumns;
     @FXML
-    TextField rows;
+    TextField golRows;
+    @FXML
+    TextField wwColumns;
+    @FXML
+    TextField wwRows;
+    @FXML
+    Button golDimension;
 
-    Board bwBoard = new Board(240,240, bwCanvas);
-    Board colorBoard = new Board(64,48, colorCanvas);
-
+    Board golBoard = new Board(240,240, golCanvas);
+    Board wwBoard = new Board(64,48, wwCanvas);
 
     public void initialize(){
 
-        //colorTool fill for WireWorld
-        GraphicsContext colorToolGC = colorTool.getGraphicsContext2D();
-        colorToolGC.setFill(Color.RED);
-        colorToolGC.fillRect(0,0, colorToolSize, colorToolSize);
-        colorToolGC.setFill(Color.YELLOW);
-        colorToolGC.fillRect(0, colorToolSize, colorToolSize, colorToolSize);
-        colorToolGC.setFill(Color.BLUE);
-        colorToolGC.fillRect(colorToolSize,0, colorToolSize, colorToolSize);
-        colorToolGC.setFill(Color.BLACK);
-        colorToolGC.fillRect(colorToolSize, colorToolSize, colorToolSize, colorToolSize);
+        //Settings for WireWorld
+        wwBoard.colorToolFill(colorTool, colorToolSize);
+        //wwBoard.canvasBlackFill(wwCanvas);
+        //wwBoard.setDimension(wwColumns, wwRows);
+        wwBoard.setPromptForDimensions(wwColumns, wwRows);
 
-        //bwToll fill for Game of Life
-        GraphicsContext bwToolGC = bwTool.getGraphicsContext2D();
-        bwToolGC.setFill(Color.BLACK);
-        bwToolGC.fillRect(0,0,bwToolSize,bwToolSize);
-        bwToolGC.setFill(Color.WHITE);
-        bwToolGC.fillRect(bwToolSize,0, bwToolSize, bwToolSize);
-
-
-        //bwCanvas fill for Game of Life
-        Random bwGenerator = new Random();
-        GraphicsContext bwCanvasGC = bwCanvas.getGraphicsContext2D();
-        bwCanvasGC.setFill(Color.BLACK);
-        for(int i = 0; i<bwBoard.getRows(); i++){
-            for(int j = 0; j<bwBoard.getColumns(); j++){
-                if(bwGenerator.nextBoolean() == true)
-                    bwCanvasGC.setFill(Color.WHITE);
-                else
-                    bwCanvasGC.setFill(Color.BLACK);
-                bwCanvasGC.fillRect(j*bwBoard.cellSize,bwBoard.cellSize*i, bwBoard.cellSize, bwBoard.cellSize);
-            }
-        }
-        //colorCanvas fill for WireWorld
-        Random colorGenerator = new Random();
-        GraphicsContext colorCanvasGC = colorCanvas.getGraphicsContext2D();
-
-        //Black fill for WireWorld
-        for(int i = 0; i<colorBoard.getRows(); i++){
-            for(int j = 0; j<colorBoard.getColumns(); j++){
-                    colorCanvasGC.setFill(Color.BLACK);
-                colorCanvasGC.fillRect(j*colorBoard.cellSize,colorBoard.cellSize*i,colorBoard.cellSize,colorBoard.cellSize);
-            }
-        }
-        /*for(int i = 0; i<24; i++){
-            for(int j = 0; j<32; j++){
-                if(colorGenerator.nextInt() % 4 == 0)
-                    colorCanvasGC.setFill(Color.RED);
-                else if(colorGenerator.nextInt() % 4 == 1)
-                    colorCanvasGC.setFill(Color.YELLOW);
-                else if(colorGenerator.nextInt() % 4 == 2)
-                    colorCanvasGC.setFill(Color.BLUE);
-                else
-                    colorCanvasGC.setFill(Color.BLACK);
-                colorCanvasGC.fillRect(j*colorBoard.cellSize,colorBoard.cellSize*i,colorBoard.cellSize,colorBoard.cellSize);
-            }
-        }*/
-
-        //Dimensions of the board
-        /*
-        if(columns != null) {
-
-            bwBoard.setColumns(Integer.parseInt(columns.getText()));
-            bwBoard.setRows(Integer.parseInt(rows.getText()));
-        }*/
+        //Settings for Game of Life
+        golBoard.bwToolFill(bwTool, bwToolSize);
+        golBoard.golCanvasFill(golCanvas);
+        //golBoard.setDimension(golColumns, golRows);
+        golBoard.setPromptForDimensions(golColumns, golRows);
 
     }
-
 }
